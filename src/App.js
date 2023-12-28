b1aeb46f6a64f33b22fa22d4dac88abcc5e73d7d
PeemSx <peemp5802@gmail.com>
2023-12-28 20:14:51 +0700

    fixed route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
+// App.js
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-
+import { SigninPage } from './pages/SiginPage';
+import { LoginPage } from './pages/LoginPage';
+import { BlogPages } from './pages/BlogPages';
+import './App.css'
 function App() {
-
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signin" element={<SigninPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/blogs" element={<BlogPages />} />
+    </Routes>
   );
 }
 
